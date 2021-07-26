745f7e2d10c60b4f3ad033727962b2b8ea624979
htdfs <53020477+blockplacer@users.noreply.github.com>
2021-07-26 04:01:14 +0300

    Remove unused code

--- Aggregator.cs
+++ Aggregator.cs
@@ -6,22 +6,7 @@ using System.Threading.Tasks;
 
 namespace RevatryFramework
 {
-    /*public static class Aggregator
-    {
 
-
-
-
-        /// <summary>
-        /// Puts every library for pages on single file so loading time and performance increases significantly
-        /// </summary>
-        /// <returns>Every library combined into one</returns>
-        public static string Aggregate(FELibType type)
-        {
-
-
-        }
-    }*/
     public enum FELibType
     {
         Css,
