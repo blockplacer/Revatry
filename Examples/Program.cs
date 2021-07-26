a34669ae8dfd81d37c6fe29f88105eeda93a5da0
htdfs <53020477+blockplacer@users.noreply.github.com>
2021-07-26 03:52:32 +0300

    Update and rename Program.cs to Examples/Program.cs

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -14,7 +14,7 @@ namespace RevatryFramework
         public static RevatryHTTP revatry = new RevatryHTTP("http://localhost:80");
 
 
-        static string bobux;
+        static string googly;
         static Database db = new Database();
         static void Main(string[] args)
         {
@@ -22,8 +22,8 @@ namespace RevatryFramework
             revatry.pages.Add(new Page("random", randomNumber));
             revatry.pages.Add(new Page("session", session));
             revatry.pages.Add(new Page("sessiongen", sessiongen));
-            revatry.pages.Add(new Page("social_experiment", googleAsync));
-            db.AddTable("sex", 50);
+            revatry.pages.Add(new Page("google", googleAsync));
+            db.AddTable("test", 50);
 
             revatry.Listen();
             revatry.ListenForPages();//testMethodGet
@@ -63,9 +63,9 @@ namespace RevatryFramework
         static string dbb = db.Serialize();
         static async void googleAsync(HttpListenerResponse res, HttpListenerRequest req)
         {
-            bobux = revatry.Request(HTTPReqs.GET, "https://discord.com");
+            googly = revatry.Request(HTTPReqs.GET, "https://google.com");
 
-            revatry.Send(bobux, res);
+            revatry.Send(googly, res);
 
             revatry.EndRequest(res);
         }
